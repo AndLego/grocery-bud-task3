f9e062c90606e29296eb9312a8979d1b6fbdb8d0
AndLego <galeon.gb@gmail.com>
2023-02-01 20:49:44 -0500

    made basic layout for the project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import GroceryCard from './components/GroceryCard'
+
+function App() {
+
+  return (
+    <GroceryCard />
+  )
+}
+
+export default App
---
+++ src/components/GroceryCard.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const GroceryCard = () => {
+  return (
+    <main>
+      <h1>Grocery Bud</h1>
+      <form>
+        <input type="text" />
+        <button>Add</button>
+      </form>
+      <div>
+        <p>some item</p>
+        <div>
+          <p>E</p>
+          <p>R</p>
+        </div>
+      </div>
+   
+    </main>
+  );
+};
+
+export default GroceryCard;
